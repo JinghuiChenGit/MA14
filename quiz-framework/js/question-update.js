0118719ed049c3c086cf6965725b9fa9fe5fcf58
Alice <tngu0157@student.monash.edu>
2020-08-27 18:45:39 +1000

    Modify quiz (change background + add explain)
    - Change the quiz background
    - Add explain
    - Add Back button (not finished)

---
+++ quiz-framework/js/question-update.js
@@ -0,0 +1,33 @@
+const quiz = [
+    {
+        q:"It is not important to save water because there is so much of it on Earth",
+        options:["True","False"],
+        answer:1,
+        explain: "75 percent of the earth's surface is water! However, most of it is salt water so you can't drink it. Less than 1% of the water on earth can be used by people! It is therefore very important to save the water."
+    },
+    {
+        q:'What should you do if you see or hear a leaky faucet in your house?',
+        options:["Ignore it","Do nothing because somebody else will see it","Tell my parents imidiately because they'll know what to do!"],
+        answer:2,         
+        explain: "Leaky faucets are very big water wasters. If you see or hear a leaky faucet, tell your parents about it so they can get it fixed. If you don't get it fixed, those drips and drops can add up to lots of wasted water!"
+    },
+    {
+        q:"Keeping the water running when you brush your teeth wastes a lot of water.",
+        options:['true','false'],
+        answer:0,
+        explain: "You can save up to 8 gallons of water by turning off the faucet when you brush your teeth."
+
+    },
+    {
+        q:"Which of the following will help you save water?",
+        options:["Leave the tap on when you brush your teeth","Flush the toilet 2 times after you use it","Have a very long bath","Have a short quick shower"],
+        answer:3,
+        explain: "You can save so much water by taking a quick shower! You get clean and save water!"
+    },
+    {
+        q:"Where does majority of plastic waste end up?",
+        options:["Oceans","Landfills","Recycled"],
+        answer:1,
+        explain: "While some of the pastic waste does end up in landfills and recycling centers, most of it reaches the ocean. This is because of deliberate dumoing of garbage in waterways, simple littering and mistakes in  disposing of waste!"
+    }
+]
