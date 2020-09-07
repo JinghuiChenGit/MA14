5596ff8ab1495524658fa4fc3f0578c627aee097
Jinghui <jche0172@student.monash.edu>
2020-09-07 11:08:50 +1000

    Iteration 2

---
+++ watersaver/watersaver/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace watersaver.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Game()
+        {
+
+            ViewBag.Message = "Game page.";
+
+            return View();
+
+
+        }
+
+
+        public ActionResult Quiz()
+        {
+
+            ViewBag.Message = "Quiz page.";
+
+            return View();
+
+
+        }
+
+
+        public ActionResult Map()
+        {
+
+            ViewBag.Message = "Map page.";
+
+            return View();
+
+
+        }
+    }
+}
---
+++ watersaver/watersaver/Scripts/question-update.js
@@ -0,0 +1,51 @@
+const quiz = [
+    {
+        q:"It is not important to save water because there is so much of it on Earth.",
+        options:["True","False"],
+        answer:1,
+        explain: "75 percent of the earth's surface is water! However, most of it is salt water so you can't drink it. Less than 1% of the water on earth can be used by people! It is therefore very important to save the water."
+    },
+    {
+        q:'What should you do if you see or hear a leaky faucet in your house?',
+        options:["Ignore it","Do nothing because somebody else will see it","Tell my parents imidiately because they'll know what to do!"],
+        answer:2,         
+        explain: "Leaky faucets are very big water wasters. If you see or hear a leaky faucet, tell your parents about it so they can get it fixed. If you don't get it fixed, those drips and drops can add up to lots of wasted water!"
+    },
+    {
+        q:"Keeping the water running when you brush your teeth wastes a lot of water.",
+        options:['true','false'],
+        answer:0,
+        explain: "You can save up to 8 gallons of water by turning off the faucet when you brush your teeth."
+
+    },
+    {
+        q:"Which of the following will help you save water?",
+        options:["Leave the tap on when you brush your teeth","Flush the toilet 2 times after you use it","Have a very long bath","Have a short quick shower"],
+        answer:3,
+        explain: "You can save so much water by taking a quick shower! You get clean and save water!"
+    },
+    {
+        q:"Where does majority of plastic waste end up?",
+        options:["Oceans","Landfills","Recycled"],
+        answer:0,
+        explain: "While some of the pastic waste does end up in landfills and recycling centers, most of it reaches the ocean. This is because of deliberate dumoing of garbage in waterways, simple littering and mistakes in  disposing of waste!"
+    },
+    {
+        q:"It's fine to litter on the street because someone will collect it eventually.",
+        options:["True","False"],
+        answer:1,
+        explain: "Litter can be carried by rainwater and wind to the lakes and rivers which harms the water catchments. It will then float to the ocean as well!"
+    },
+    {
+        q:"Which option(s) below that you can do would help save the ocean from plastics?",
+        options:["Sorting the recycled plastics and putting them in recycle bins","Avoiding flushing sanitary products or anything except toilet paper down toilets","Putting the trash in the bin", "All the options are correct."],
+        answer:3,
+        explain: "We can do so many things to help the ocean! Recycled plastics can end up being in the landfill if we don't put them in recycle bins and is often blow away to the ocean. Any sanitary products except flushable toiler paper can be missed during the water treatment process and travel to the ocean. Putting the trash in the bin is always an option to protect the environment in general. "
+    },
+    {
+        q:"Protecting the water streams is an adult task because I cannot do anything about it.",
+        options:["True","False"],
+        answer:1,
+        explain: "You can always do small things to protect our water streams everyday! Just rememeber to put the trash to the pin, recycle or reuse the plastics whenever you can, and advoid flushing not flushable items down the toilets."
+    }
+]
